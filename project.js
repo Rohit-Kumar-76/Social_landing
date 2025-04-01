32af037fc172a97854aba5e614208e484711a246
Rohit Kumar <148522757+Rohit-Kumar-76@users.noreply.github.com>
2025-04-01 17:16:30 +0530

    File upload

---
+++ project.js
@@ -0,0 +1,16 @@
+function validate()
+{
+    var emai=document.getElementById("email").value;
+    var password=document.getElementById("password").value;
+    char admin;
+    char user;
+    if (email==admin && password==user)
+    {
+        alert("Login succesfully");
+    
+    }
+    else
+    {
+        alert("login Failed");
+    }
+}
